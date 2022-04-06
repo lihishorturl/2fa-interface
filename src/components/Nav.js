cf2437305c05f5c7453cf0a5e400cb86bae8d263
Falcon Ho <endless640c@gmail.com>
2022-04-06 15:30:53 +0800

    feat(nav): update links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,8 +6,8 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline'
 const navigation = [
   { name: 'Our Service ', href: window.location.href + '#features' },
   { name: 'Our Team ', href: 'https://knowledge.lihi.io/about-us/' },
-  { name: 'BSC Contract', href: 'https://bscscan.com/' },
-  { name: 'Github', href: 'https://github.com/' },
+  { name: 'BSC Contract', href: 'https://bscscan.com/address/0x9945ca848b2f9ffe8f708989d157baa6a533c8e0' },
+  { name: 'Github', href: 'https://github.com/lihishorturl?tab=repositories' },
 ]
 
 export default function Nav({ defaultAccount }) {
